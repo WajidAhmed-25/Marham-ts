0be35c29d85a912c7bd7588eecaf2d3c64cd2fcd
Billal-07 <bilhashmi@icloud.com>
2023-11-05 21:49:24 +0500

    DocLogin Signup Created

--- src/components/Country_Code/country.tsx
+++ src/components/Country_Code/country.tsx
@@ -1,79 +1,67 @@
-
-
-
-export default function Country(){
-
-
-    return(
-     
-
-<div className="absolute mt-[-38px] ml-1 ">  
- <select id="countryCode" name="countryCode" className="block py-1 pl-2 pr-8 leading-tight text-blue-400 bg-blue-200 border-2 border-white rounded-md appearance-none focus:outline-none focus:shadow-outline">
-    <option value="" className="hover:bg-white" >Code</option>
-    <option value="+1"  className="bg-white">+1</option>
-    <option value="+44" className="bg-white">+44</option>
-    <option value="+33" className="bg-white">+33</option>
-    <option value="+49" className="bg-white">+49</option>
-    <option value="+86" className="bg-white">+86</option>
-    <option value="+91" className="bg-white">+91</option>
-    <option value="+81" className="bg-white">+81</option>
-    <option value="+7" className="bg-white">+7</option>
-    <option value="+61" className="bg-white">+61</option>
-    <option value="+55" className="bg-white">+55</option>
-    <option value="+1" className="bg-white">+1</option>
-    <option value="+52" className="bg-white">+52</option>
-    <option value="+34" className="bg-white">+34</option>
-    <option value="+39" className="bg-white">+39</option>
-    <option value="+44" className="bg-white">+44</option>
-    <option value="+353" className="bg-white">+353</option>
-    <option value="+31" className="bg-white">+31</option>
-    <option value="+32" className="bg-white">+32</option>
-    <option value="+41" className="bg-white">+41</option>
-    <option value="+46" className="bg-white">+46</option>
-    <option value="+47" className="bg-white">+47</option>
-    <option value="+45" className="bg-white">+45</option>
-    <option value="+64" className="bg-white">+64</option>
-    <option value="+65" className="bg-white">+65</option>
-    <option value="+82" className="bg-white">+82</option>
-    <option value="+81" className="bg-white">+81</option>
-    <option value="+86" className="bg-white">+86</option>
-    <option value="+852" className="bg-white">+852</option>
-    <option value="+886" className="bg-white">+886</option>
-    <option value="+7" className="bg-white">+7</option>
-    <option value="+380" className="bg-white">+380</option>
-    <option value="+90" className="bg-white">+90</option>
-    <option value="+30" className="bg-white">+30</option>
-    <option value="+54" className="bg-white">+54</option>
-    <option value="+55" className="bg-white">+55</option>
-    <option value="+56" className="bg-white">+56</option>
-    <option value="+57" className="bg-white">+57</option>
-    <option value="+51" className="bg-white">+51</option>
-    <option value="+52" className="bg-white">+52</option>
-    <option value="+502" className="bg-white">+502</option>
-    <option value="+506" className="bg-white">+506</option>
-    <option value="+503" className="bg-white">+503</option>
-    <option value="+504" className="bg-white">+504</option>
-    <option value="+505" className="bg-white">+505</option>
-    <option value="+506" className="bg-white">+506</option>
-    <option value="+52" className="bg-white">+52</option>
-    <option value="+503" className="bg-white">+503</option>
-    <option value="+504" className="bg-white">+504</option>
-    <option value="+972" className="bg-white">+972</option>
-    <option value="+966" className="bg-white">+966</option>
-    <option value="+971" className="bg-white">+971</option>
-    <option value="+20" className="bg-white">+20</option>
-    <option value="+86" className="bg-white">+86</option>
-    <option value="+82" className="bg-white">+82</option>
-    <option value="+81" className="bg-white">+81</option>
-    <option value="+63" className="bg-white">+63</option>
-    <option value="+65" className="bg-white">+65</option>
-    <option value="+60" className="bg-white">+60</option>
-  </select>
-
-  {/* <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
-    <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"></path></svg>
-  </div> */}
-
-        </div>
-    )
+export default function Country() {
+  return (
+    <div className="absolute mt-[-38px] ml-1 ">
+      <select id="countryCode" name="countryCode" className="block py-1 pl-2 pr-8 leading-tight text-blue-400 bg-blue-200 border-2 border-white rounded-md appearance-none focus:outline-none focus:shadow-outline">
+        <option value="" className="hover:bg-white" >Code</option>
+        <option value="+1" className="bg-white">+1</option>
+        <option value="+44" className="bg-white">+44</option>
+        <option value="+33" className="bg-white">+33</option>
+        <option value="+49" className="bg-white">+49</option>
+        <option value="+86" className="bg-white">+86</option>
+        <option value="+91" className="bg-white">+91</option>
+        <option value="+81" className="bg-white">+81</option>
+        <option value="+7" className="bg-white">+7</option>
+        <option value="+61" className="bg-white">+61</option>
+        <option value="+55" className="bg-white">+55</option>
+        <option value="+1" className="bg-white">+1</option>
+        <option value="+52" className="bg-white">+52</option>
+        <option value="+34" className="bg-white">+34</option>
+        <option value="+39" className="bg-white">+39</option>
+        <option value="+44" className="bg-white">+44</option>
+        <option value="+353" className="bg-white">+353</option>
+        <option value="+31" className="bg-white">+31</option>
+        <option value="+32" className="bg-white">+32</option>
+        <option value="+41" className="bg-white">+41</option>
+        <option value="+46" className="bg-white">+46</option>
+        <option value="+47" className="bg-white">+47</option>
+        <option value="+45" className="bg-white">+45</option>
+        <option value="+64" className="bg-white">+64</option>
+        <option value="+65" className="bg-white">+65</option>
+        <option value="+82" className="bg-white">+82</option>
+        <option value="+81" className="bg-white">+81</option>
+        <option value="+86" className="bg-white">+86</option>
+        <option value="+852" className="bg-white">+852</option>
+        <option value="+886" className="bg-white">+886</option>
+        <option value="+7" className="bg-white">+7</option>
+        <option value="+380" className="bg-white">+380</option>
+        <option value="+90" className="bg-white">+90</option>
+        <option value="+30" className="bg-white">+30</option>
+        <option value="+54" className="bg-white">+54</option>
+        <option value="+55" className="bg-white">+55</option>
+        <option value="+56" className="bg-white">+56</option>
+        <option value="+57" className="bg-white">+57</option>
+        <option value="+51" className="bg-white">+51</option>
+        <option value="+52" className="bg-white">+52</option>
+        <option value="+502" className="bg-white">+502</option>
+        <option value="+506" className="bg-white">+506</option>
+        <option value="+503" className="bg-white">+503</option>
+        <option value="+504" className="bg-white">+504</option>
+        <option value="+505" className="bg-white">+505</option>
+        <option value="+506" className="bg-white">+506</option>
+        <option value="+52" className="bg-white">+52</option>
+        <option value="+503" className="bg-white">+503</option>
+        <option value="+504" className="bg-white">+504</option>
+        <option value="+972" className="bg-white">+972</option>
+        <option value="+966" className="bg-white">+966</option>
+        <option value="+971" className="bg-white">+971</option>
+        <option value="+20" className="bg-white">+20</option>
+        <option value="+86" className="bg-white">+86</option>
+        <option value="+82" className="bg-white">+82</option>
+        <option value="+81" className="bg-white">+81</option>
+        <option value="+63" className="bg-white">+63</option>
+        <option value="+65" className="bg-white">+65</option>
+        <option value="+60" className="bg-white">+60</option>
+      </select>
+    </div>
+  )
 }
